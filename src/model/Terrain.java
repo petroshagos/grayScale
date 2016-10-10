b328d7b6d29f3a63e81d8a8f2aa135e6695f16af
Petros <petros.h.hagos@gmail.com>
2016-10-10 17:05:33 +0200

    new commit

--- src/model/Terrain.java
+++ src/model/Terrain.java
@@ -3,5 +3,26 @@ package model;
 /**
  * Created by Petros on 2016-10-09.
  */
-public class Terrain {
+public class Terrain extends Shape {
+
+    private double[] xPoints, yPoints;
+
+    public Terrain(double x, double y, int color, boolean filled,
+                   boolean outOfBounds, boolean collidable, double[] xPoints, double[] yPoints) {
+        super(x,y,color,filled,outOfBounds,collidable);
+        this.xPoints = xPoints;
+        this.yPoints = yPoints;
+    }
+
+    public double[] getxPoints() {
+        return xPoints;
+    }
+
+    public double[] getyPoints() {
+        return yPoints;
+    }
+
+    public double[] getLastPoint() {
+        return (new double[]{this.xPoints[xPoints.length-3], this.yPoints[yPoints.length-3]});
+    }
 }
