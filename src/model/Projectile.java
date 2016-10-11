6400d67530a3afb39184c39196649132eee2cfe0
Petros <petros.h.hagos@gmail.com>
2016-10-11 08:38:47 +0200

    new commit

--- src/model/Projectile.java
+++ src/model/Projectile.java
@@ -1,7 +1,26 @@
 package model;
 
+import java.util.LinkedList;
+
 /**
  * Created by Petros on 2016-10-09.
  */
 public class Projectile {
+
+    private LinkedList<Shape> projectile;
+
+    public Projectile(double x, double y) {
+        for (int i=0;i<3;i++){
+            for (int j=0;j<2;j++){
+                this.projectile.add(new Rectangle(x+6*i,y+6*j,6,6,4,true,false,true));
+            }
+        }
+        for (Shape s: this.projectile) {
+            s.setVelocity(40,0);
+        }
+    }
+
+    public LinkedList<Shape> getProjectile() {
+        return this.projectile;
+    }
 }
--- src/view/ThemeColor.java
+++ src/view/ThemeColor.java
@@ -4,9 +4,12 @@ package view;
  * Created by Petros on 2016-10-09.
  */
 public enum ThemeColor {
-    THEME_GRAY(new int[] {50,50,50}, new int[] {125,125,125}, new int[] {200,200,200}, new int[] {160,160,160}, new int[] {255,255,255}),
-    THEME_BLUE(new int[] {0,26,68}, new int[] {67, 94, 137}, new int[] {106, 154, 232}, new int[] {53, 104, 186}, new int[] {255,255,255}),
-    THEME_RED(new int[] {73, 3, 11}, new int[] {160, 85, 93}, new int[] {234, 53, 75}, new int[] {211, 91, 105}, new int[] {255,255,255});
+    THEME_GRAY(new int[] {50,50,50}, new int[] {125,125,125}, new int[] {200,200,200},
+            new int[] {160,160,160}, new int[] {255,255,255}),
+    THEME_BLUE(new int[] {0,26,68}, new int[] {67, 94, 137}, new int[] {106, 154, 232},
+            new int[] {53, 104, 186}, new int[] {255,255,255}),
+    THEME_RED(new int[] {73, 3, 11}, new int[] {160, 85, 93}, new int[] {234, 53, 75},
+            new int[] {211, 91, 105}, new int[] {255,255,255});
 
 
     public int[] getColor1() {
