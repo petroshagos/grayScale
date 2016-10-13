7c2ded6b55360095dbbcc928ddfdd5f1461f8796
Micke <Micke@130.229.145.198>
2016-10-13 14:21:55 +0200

    Tester

--- src/view/FX/TerrainFX.java
+++ src/view/FX/TerrainFX.java
@@ -21,6 +21,13 @@ public class TerrainFX extends ShapeFX {
         gc.setFill(super.getThemeColor().getColor(terrain.getColor()));
         gc.setStroke(super.getThemeColor().getColor(terrain.getColor()));
         if (terrain.isFilled()) {
+            for (int i = 0; i < terrain.getXPoints().length; i++) {
+                System.out.println("xpoint: " + i + ": " + terrain.getXPoints()[i]);
+            }
+            for (int i = 0; i < terrain.getYPoints().length; i++) {
+                System.out.println("ypoint: "+i+": "+terrain.getYPoints()[i]);   
+            }
+            System.out.println("nrofpoints: "+terrain.getNrOfPoints());
             gc.fillPolygon(terrain.getXPoints(), terrain.getYPoints(), terrain.getNrOfPoints());
         }
         else {
